cacb9f76d9f03226c74d104dd39cea18865ac039
badari07 <badari0793@gmail.com>
2020-05-15 17:35:47 +0530

    feat: adding CRUD OP

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,28 @@
-import { crudControllers } from '../../utils/crud'
-import { Item } from './item.model'
+import { Item } from "./item.model";
+import mongoose from "mongoose";
+import { connect } from "../../utils/db";
+import { update } from "lodash";
 
-export default crudControllers(Item)
+const run = async () => {
+  await connect("mongodb://localhost:27017/api-test");
+  const item = await Item.create({
+    name: "Clean up",
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId(),
+  });
+
+  //console.log(await Item.findById(item._id).exec());
+  //console.log(item);
+  //   const update = await Item.findByIdAndUpdate(
+  //     item._id,
+  //     { name: "bad" },
+  //     { new: true }
+  //   ).exec();
+
+  //   console.log(update);
+
+  const remove = await Item.findByIdAndRemove(item._id).exec();
+  console.log(remove);
+};
+
+run();
